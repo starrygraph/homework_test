2c8ec4ebb61f9f2646a8f4295f2b8a4de1ccea8a
starrygraph <102165754+starrygraph@users.noreply.github.com>
2022-06-17 21:37:49 +0800

    Add files via upload

---
+++ gcn_utils.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+class VertexLayer(object):
+    '''
+      Wrapper of GCN's vertex functions
+    '''
+
+    def __init__(self, layer, nn):
+        self.layer = layer
+        self.nn = nn
+
+    def build(self, inputs, num_neighbors, num_filters, neigh_idx, scope=None, is_training=None):
+        vertex_features = self.layer(inputs,
+                                     neigh_idx,
+                                     self.nn,
+                                     num_neighbors,
+                                     num_filters,
+                                     scope=scope,
+                                     is_training=is_training)
+        return vertex_features
+
+
+class EdgeLayer(object):
+    '''
+      Wrapper of GCN's edge functions
+    '''
+
+    def __init__(self, layer, distance_metric):
+        self.layer = layer
+        self.distance_metric = distance_metric
+
+    def build(self, inputs, num_neighbors, dilation=None, is_training=None):
+        if self.layer.__name__ == 'knn_graph':
+            neigh_idx = self.layer(inputs,
+                                   num_neighbors,
+                                   distance_metric=self.distance_metric)
+        elif self.layer.__name__ == 'dilated_knn_graph':
+            neigh_idx = self.layer(inputs,
+                                   num_neighbors,
+                                   distance_metric=self.distance_metric,
+                                   dilation=dilation,
+                                   is_training=is_training)
+        else:
+            raise Exception('Unknown layer function {}'.format(self.layer.__name__))
+
+        return neigh_idx
